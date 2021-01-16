540ca03a5b6338779e3c1d8f75f6ed22a434b3c3
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-01-15 22:08:12 -0500

    with new made up

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('style',function() {
-    gulp.src('style.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
-});
